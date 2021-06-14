4ae1d66bcc608c096d019a037fb4a313a12fbfcc
OE.Heart <leoouyixin@126.com>
2021-06-14 23:39:40 +0800

    Update CatalogManager

--- CatalogManager/CatalogManager.cpp
+++ CatalogManager/CatalogManager.cpp
@@ -2,7 +2,7 @@
  * @Author: Ou Yixin
  * @Date: 2021-06-14 17:41:38
  * @LastEditors: Ou Yixin
- * @LastEditTime: 2021-06-14 19:54:08
+ * @LastEditTime: 2021-06-14 20:46:54
  * @Description: 
  * @FilePath: /MiniSQL/CatalogManager/CatalogManager.cpp
  */
@@ -16,6 +16,58 @@ CatalogManager::CatalogManager()
 CatalogManager::~CatalogManager()
 {
     
+}
+
+void CatalogManager::newTable(const std::string &tableName, const std::vector<Column> &columns)
+{
+    tables[tableName] = Table{tableName, columns};
+}
+
+void CatalogManager::newIndex(const std::string &indexName, const std::string &tableName, const std::string &columnName)
+{
+    indices[indexName] = Index{indexName, tableName, columnName};
+    auto &table = tables[tableName];
+    for (auto &column : table.columns) 
+    {
+        if (column.columnName == columnName) 
+        {
+            column.index = columnName;
+            break;
+        }
+    }
+}
+
+void CatalogManager::dropTable(const std::string &tableName) 
+{
+    auto table = tables[tableName];
+    tables.erase(tableName);
+    for (auto column : table.columns) 
+    {
+        if (column.index != "") 
+        {
+            indices.erase(column.index);
+        }
+    }
+}
+
+void CatalogManager::dropIndex(const std::string &indexName) 
+{
+    auto index = indices[indexName];
+    indices.erase(indexName);
+    auto table = tables[index.tableName];
+    for (auto &column : table.columns) 
+    {
+        if (column.columnName == index.columnName) 
+        {
+            column.index = "";
+            break;
+        }
+    }
+}
+
+bool CatalogManager::validName(const std::string &name) const 
+{
+    return tables.count(name) == 0 && indices.count(name);
 }
 
 void CatalogManager::load()
--- CatalogManager/CatalogManager.hpp
+++ CatalogManager/CatalogManager.hpp
@@ -2,20 +2,34 @@
  * @Author: Ou Yixin
  * @Date: 2021-06-14 17:42:22
  * @LastEditors: Ou Yixin
- * @LastEditTime: 2021-06-14 19:53:50
+ * @LastEditTime: 2021-06-14 20:49:24
  * @Description: 
  * @FilePath: /MiniSQL/CatalogManager/CatalogManager.hpp
  */
 #pragma once
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "Table.hpp"
+#include "Column.hpp"
+#include "Index.hpp"
 
 class CatalogManager{
 public:
     CatalogManager();
     ~CatalogManager();
 
+    void newTable(const std::string &tableName, const std::vector<Column> &columns);
+    void newIndex(const std::string &indexName, const std::string &tableName, const std::string &columnName);
+    void dropTable(const std::string &tableName);
+    void dropIndex(const std::string &indexName);
+    bool validName(const std::string &name) const;
+    
     void load();
     void save();
 
 private:
-    
+    std::unordered_map<std::string, Table> tables;
+    std::unordered_map<std::string, Index> indices;
 };
